248601d8f9676e9ff98c2d06c79cada4fac79d7c
Lean <leian1306@gmail.com>
2024-12-01 12:47:29 +0100

    Explorer: add initial markup with basic styles

--- src/json-explorer/json-explorer.tsx
+++ src/json-explorer/json-explorer.tsx
@@ -1,7 +1,28 @@
+import './json-explorer.css';
+
 interface Props {
     jsonObject: object;
 }
 
 export default function JsonExplorer({ jsonObject }: Props) {
-    return <pre>{jsonObject.toString()}</pre>;
+    return (
+        <article className="json-explorer">
+            <form name="json-explorer" className="form">
+                <div className="form-input">
+                    <label htmlFor="property">Property</label>
+                    <input type="text" name="property" placeholder="Property" />
+                </div>
+                <p className="property-value">
+                    Property Value
+                </p>
+            </form>
+
+            <section>
+                <p><strong>Response</strong></p>
+                <pre className="json-content">
+                    {jsonObject.toString()}
+                </pre>
+            </section>
+        </article>
+    );
 }
